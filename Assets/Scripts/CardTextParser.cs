470e024d34d883ef0685fa5f1763c29ed1407490
Westley Waligora <westleywaligora@gmail.com>
2018-08-16 15:29:22 -0400

    Added a button to the UI that allows you to screenshot and export all of the cards Headers in the excel sheet no longer appear as cards in the dropdown The first card generated is auto selected when you start the program

--- Assets/Scripts/CardTextParser.cs
+++ Assets/Scripts/CardTextParser.cs
@@ -7,70 +7,95 @@ using System.IO;
 
 public class CardTextParser : MonoBehaviour
 {
-	public string path = "Assets/cardFile.csv";
-	public DisplayCard cardUI;
-	public Dropdown cardDropdown;
-	List<SpringsCard> cards = new List<SpringsCard>();
+    public string path = "Assets/cardFile.csv";
+    public DisplayCard cardUI;
+    public Dropdown cardDropdown;
+    List<SpringsCard> cards = new List<SpringsCard>();
 
-	// Use this for initialization
-	void Start ()
-	{
-		StreamReader reader = new StreamReader(path);
-		string line;
-		List<string> cardNames = new List<string>();
+    // Use this for initialization
+    void Start()
+    {
+        StreamReader reader = new StreamReader(path);
+        string line;
+        List<string> cardNames = new List<string>();
 
-		while((line = reader.ReadLine()) != null)
-		{
-			cardNames.Add(GenerateCard(line));
-		}
+        while ((line = reader.ReadLine()) != null)
+        {
+            string cardName = GenerateCard(line);
 
-		cardDropdown.AddOptions(cardNames);
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		
-	}
+            if (cardName != null)
+            {
+                cardNames.Add(cardName);
+            }
+        }
 
-	public string GenerateCard(string cardText)
-	{
-		string[] tokenizedText = cardText.Split(',');
-		SpringsCard card;
+        cardDropdown.AddOptions(cardNames);
+        cardDropdown.RefreshShownValue();
+        OnCardSelected(0);
+    }
 
-		if (tokenizedText[1] == CardType.Unit.ToString())
-		{
-			Unit unit = new Unit();
-			unit.attack = tokenizedText[4];
-			unit.health = tokenizedText[5];
-			//unit.attackType = (AttackType)Enum.Parse(typeof(AttackType), tokenizedText[6]);
-			card = unit;
-			//card.type = CardType.Unit;
-		}
-		else if(tokenizedText[1] == CardType.Spell.ToString())
-		{
-			card = new Spell();
-		}
-		else if(tokenizedText[1] == CardType.Worker.ToString())
-		{
-			card = new Worker();
-		}
-		else
-		{
-			card = new Unit();
-		}
+    // Update is called once per frame
+    void Update()
+    {
 
-		card.name = tokenizedText[0];
-		card.goldCost = tokenizedText[2];
-		card.manaCost = tokenizedText[3];
-		card.effect = tokenizedText[7];
-		cards.Add(card);
+    }
 
-		return card.name;
-	}
+    public string GenerateCard(string cardText)
+    {
+        string[] tokenizedText = cardText.Split(',');
+        SpringsCard card;
+        bool addCard = true;
 
-	public void OnCardSelected(int selectionIndex)
-	{
-		cardUI.ActivateCardImage(cards[selectionIndex]);
-	}
+        if (tokenizedText[1] == CardType.Unit.ToString())
+        {
+            Unit unit = new Unit();
+            unit.attack = tokenizedText[4];
+            unit.health = tokenizedText[5];
+            //unit.attackType = (AttackType)Enum.Parse(typeof(AttackType), tokenizedText[6]);
+            card = unit;
+            //card.type = CardType.Unit;
+        }
+        else if (tokenizedText[1] == CardType.Spell.ToString())
+        {
+            card = new Spell();
+        }
+        else if (tokenizedText[1] == CardType.Worker.ToString())
+        {
+            card = new Worker();
+        }
+        else
+        {
+            //If tokenizedText[1] doesn't equal one of the other ifs then the line is probably one of the headers
+            card = new Unit();
+            addCard = false;
+        }
+
+        card.name = tokenizedText[0];
+        card.goldCost = tokenizedText[2];
+        card.manaCost = tokenizedText[3];
+        card.effect = tokenizedText[7];
+
+        if (addCard)
+        {
+            cards.Add(card);
+            return card.name;
+        }
+
+        return null;
+    }
+
+    public void OnCardSelected(int selectionIndex)
+    {
+        cardUI.ActivateCardImage(cards[selectionIndex]);
+    }
+
+    public int GetNumOfCards()
+    {
+        return cards.Count;
+    }
+
+    public SpringsCard GetCard(int cardIndex)
+    {
+        return cards[cardIndex];
+    }
 }
---
+++ Assets/Scripts/Screenshot.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class Screenshot : MonoBehaviour
+{
+    public string screenshotRelativeFilepath;
+    public RectTransform cardTransform;
+    public DisplayCard cardTemplate;
+    public CardTextParser allCards;
+    bool isScreenshotting = false;
+
+    public void Start()
+    {
+        if(!cardTransform)
+        {
+            Debug.LogError("The card transform was not set, unable to find the image to screenshot!");
+        }
+
+        if (!cardTemplate)
+        {
+            Debug.LogError("The card template was not set, unable to change images!");
+        }
+
+        if (!allCards)
+        {
+            Debug.LogError("The card transform was not set, unable to get the card list!");
+        }
+    }
+
+    public void ScreenshotAllCards()
+    {
+        StartCoroutine(ScreenshotAll());
+        Debug.Log("Screenshot all images");
+    }
+
+    IEnumerator SaveCardImage(string relativeFilepath, string fileName)
+    {
+        //Wait for end of screen buffer
+        yield return new WaitForEndOfFrame();
+        Debug.Log("Screenshotting " + relativeFilepath);
+
+        //Create new texture of the specified size
+        //Debug.Log("The width is " + cardTransform.rect.width + " and the height is " + cardTransform.rect.height);
+        Texture2D tex = new Texture2D((int)cardTransform.rect.width, (int)cardTransform.rect.height, TextureFormat.RGB24, false);
+
+        //float textX = FindObjectOfType<DisplayCard>().GetComponent<RectTransform>().offsetMin.x;
+
+        // Read screen contents into the texture
+        tex.ReadPixels(new Rect(cardTransform.position.x, cardTransform.position.y, cardTransform.rect.width, cardTransform.rect.height), 0, 0);
+        tex.Apply();
+
+        // Encode texture into PNG
+        byte[] bytes = tex.EncodeToPNG();
+        Object.Destroy(tex);
+        string path = Path.Combine(Application.dataPath, relativeFilepath);
+
+        try
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+        catch (IOException ex)
+        {
+            Debug.LogError("Error message: " + ex.Message);
+        }
+
+        //Write to a file in the project folder
+        File.WriteAllBytes(Path.Combine(path, fileName), bytes);
+    }
+
+    IEnumerator ScreenshotAll()
+    {
+        isScreenshotting = true;
+
+        for(int i = 0;i<allCards.GetNumOfCards();i++)
+        {
+            SpringsCard card = allCards.GetCard(i);
+            cardTemplate.ActivateCardImage(card);
+            StartCoroutine(SaveCardImage(screenshotRelativeFilepath, card.name + ".png"));
+            yield return new WaitForEndOfFrame();
+        }
+
+        isScreenshotting = false;
+    }
+}
